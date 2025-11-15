ab799dd0a202944c81d0bf37513d0bfe8ac8557d
Shagor <infinityshagor18557@gmail.com>
2025-11-15 21:38:04 +0600

    client setup

---
+++ client/src/routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>hello</div>,
+  },
+]);
+
+export default router;
